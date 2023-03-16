e186dafbd21fbea068ba30e878f415736129d0d3
ShamarcusW-MAC <shamarcusw84@gmail.com>
2023-03-16 13:19:07 -0400

    Initial Commit

---
+++ NYCSchools-SW/NYCSchools_SWApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchools_SWApp.swift
+//  NYCSchools-SW
+//
+//  Created by Sha'Marcus Walker on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchools_SWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
